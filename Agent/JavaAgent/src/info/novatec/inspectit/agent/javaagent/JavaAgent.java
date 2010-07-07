10180e8f8fffef4379b723094cf5170736e132b9
Patrice Bouillet <patrice.bouillet@novatec-gmbh.de>
2010-07-07 09:36:29 +0200

    INSPECTIT-255: added some preconditions to the javaagent to prevent errors from happening

--- Agent/JavaAgent/src/info/novatec/inspectit/agent/javaagent/JavaAgent.java
+++ Agent/JavaAgent/src/info/novatec/inspectit/agent/javaagent/JavaAgent.java
@@ -9,11 +9,11 @@ import java.security.ProtectionDomain;
 import java.util.logging.Logger;
 
 /**
- * The JavaAgent is used since Java 5.0 to instrument classes before they are
- * actually loaded by the VM.
+ * The JavaAgent is used since Java 5.0 to instrument classes before they are actually loaded by the
+ * VM.
  * 
- * This method is used by specifying the -javaagent attribute on the command
- * line. Example: <code>-javaagent:inspecit-agent.jar</code>
+ * This method is used by specifying the -javaagent attribute on the command line. Example:
+ * <code>-javaagent:inspecit-agent.jar</code>
  * 
  * @author Patrice Bouillet
  * 
@@ -25,12 +25,15 @@ public class JavaAgent implements ClassFileTransformer {
 	 */
 	private static final Logger LOGGER = Logger.getLogger(JavaAgent.class.getName());
 
+	/**
+	 * The start patterns to ignore. This will prevent the agent from loading while core java
+	 * classes are loaded.
+	 */
 	private static final String[] IGNORE_START_PATTERNS = new String[] { "java/", "javax/", "sun/" };
 
 	/**
-	 * In case that multiple classes are loaded at the same time, which happens
-	 * in some cases, even though the JVM specification prohibits that (the case
-	 * at hand was starting ant)
+	 * In case that multiple classes are loaded at the same time, which happens in some cases, even
+	 * though the JVM specification prohibits that (the case at hand was starting ant)
 	 */
 	private static volatile boolean operationInProgress = false;
 
@@ -40,8 +43,8 @@ public class JavaAgent implements ClassFileTransformer {
 	 * @param agentArgs
 	 *            Some arguments.
 	 * @param inst
-	 *            The instrumentation instance is used to add a transformer
-	 *            which will do the actual instrumentation.
+	 *            The instrumentation instance is used to add a transformer which will do the actual
+	 *            instrumentation.
 	 */
 	public static void premain(String agentArgs, Instrumentation inst) {
 		inst.addTransformer(new JavaAgent());
@@ -52,6 +55,16 @@ public class JavaAgent implements ClassFileTransformer {
 	 */
 	public byte[] transform(final ClassLoader classLoader, final String className, final Class<?> clazz, final ProtectionDomain pd, final byte[] data) throws IllegalClassFormatException {
 		try {
+			// early return if some conditions fail
+			if (null == data || data.length == 0 || null == className || "".equals(className) || null == classLoader) {
+				// - no data = we cannot construct the class and analyze it
+				// - no class name = we don't know how the name of the class is and so the whole
+				// analysis will fail
+				// - no classloader = the bootstrap classloader tries to load something, we don't
+				// care about these classes for now
+				return data;
+			}
+
 			// ignore all classes which fit to these patterns, prevents the
 			// early loading of the inspectit agent.
 			for (int i = 0; i < IGNORE_START_PATTERNS.length; i++) {
@@ -61,6 +74,7 @@ public class JavaAgent implements ClassFileTransformer {
 				}
 			}
 
+			// now the real inspectit agent will handle this class
 			if (!operationInProgress) {
 				operationInProgress = true;
 				String modifiedClassName = className.replaceAll("/", ".");
