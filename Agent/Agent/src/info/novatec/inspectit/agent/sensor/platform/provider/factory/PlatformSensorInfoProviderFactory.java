9ef2898d52c9d1fc907fbce6f25e268713809844
Ivan Senic <ivan.senic@novatec-gmbh.de>
2012-03-12 12:01:46 +0100

    INSPECTIT-634: Support IBM JVM on the Agent for System Information

--- Agent/Agent/src/info/novatec/inspectit/agent/sensor/platform/provider/factory/PlatformSensorInfoProviderFactory.java
+++ Agent/Agent/src/info/novatec/inspectit/agent/sensor/platform/provider/factory/PlatformSensorInfoProviderFactory.java
@@ -2,9 +2,14 @@ package info.novatec.inspectit.agent.sensor.platform.provider.factory;
 
 import info.novatec.inspectit.agent.sensor.platform.provider.PlatformSensorInfoProvider;
 import info.novatec.inspectit.agent.sensor.platform.provider.def.DefaultPlatformSensorInfoProvider;
+import info.novatec.inspectit.agent.sensor.platform.provider.ibm.IbmJava6PlatformSensorInfoProvider;
 import info.novatec.inspectit.agent.sensor.platform.provider.sun.SunPlatformSensorInfoProvider;
 import info.novatec.inspectit.util.UnderlyingSystemInfo;
+import info.novatec.inspectit.util.UnderlyingSystemInfo.JavaVersion;
 import info.novatec.inspectit.util.UnderlyingSystemInfo.JvmProvider;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class decides which {@link PlatformSensorInfoProvider} will be used.
@@ -15,14 +20,44 @@ import info.novatec.inspectit.util.UnderlyingSystemInfo.JvmProvider;
 public final class PlatformSensorInfoProviderFactory {
 
 	/**
-	 * {@link PlatformSensorInfoProvider} when the agent is running on the Sun JVM.
+	 * The logger of the class.
 	 */
-	private static volatile SunPlatformSensorInfoProvider sunPlatformSensorInfoProvider;
+	private static final Logger LOGGER = Logger.getLogger(PlatformSensorInfoProviderFactory.class.getName());
 
 	/**
-	 * {@link PlatformSensorInfoProvider} when the agent is nopt running on the Sun JVM.
+	 * {@link PlatformSensorInfoProvider} when the agent is running on the Sun JVM.
 	 */
-	private static volatile DefaultPlatformSensorInfoProvider defaultPlatformSensorInfoProvider;
+	private static volatile PlatformSensorInfoProvider platformSensorInfoProvider;
+
+	/**
+	 * Because the factory should always return same implementation of the
+	 * {@link PlatformSensorInfoProvider} for one virtual machine, we can initialize the correct one
+	 * in the static class initialization, and then simply always return the one we have created.
+	 */
+	static {
+		if (UnderlyingSystemInfo.JVM_PROVIDER == JvmProvider.SUN) {
+			if (null == platformSensorInfoProvider) {
+				createSunPlatformSensorInfoProvider();
+			}
+		} else if (UnderlyingSystemInfo.JVM_PROVIDER == JvmProvider.IBM && (UnderlyingSystemInfo.JAVA_VERSION == JavaVersion.JAVA_1_6 || UnderlyingSystemInfo.JAVA_VERSION == JavaVersion.JAVA_1_7)) {
+			boolean loaded = true;
+			if (null == platformSensorInfoProvider) {
+				try {
+					createIbmJava6PlatformSensorInfoProvider();
+				} catch (Exception e) {
+					loaded = false;
+					LOGGER.log(Level.WARNING, "Creation of the Platform Sensor Info Provider for IBM virtual machine failed.", e);
+				}
+			}
+			if (loaded) {
+			}
+		}
+
+		// if nothing is returned, the go for the default
+		if (null == platformSensorInfoProvider) {
+			createDefaultPlatformSensorInfoProvider();
+		}
+	}
 
 	/**
 	 * Private constructor.
@@ -37,16 +72,18 @@ public final class PlatformSensorInfoProviderFactory {
 	 * @see UnderlyingSystemInfo
 	 */
 	public static PlatformSensorInfoProvider getPlatformSensorInfoProvider() {
-		if (UnderlyingSystemInfo.JVM_PROVIDER == JvmProvider.SUN) {
-			if (null == sunPlatformSensorInfoProvider) {
-				createSunPlatformSensorInfoProvider();
-			}
-			return sunPlatformSensorInfoProvider;
-		} else {
-			if (null == defaultPlatformSensorInfoProvider) {
-				createDefaultPlatformSensorInfoProvider();
-			}
-			return defaultPlatformSensorInfoProvider;
+		return platformSensorInfoProvider;
+	}
+
+	/**
+	 * Creates the {@link IbmJava6PlatformSensorInfoProvider}.
+	 * 
+	 * @throws Exception
+	 *             If the IBM platfrom sensor creation throws an exception.
+	 */
+	private static synchronized void createIbmJava6PlatformSensorInfoProvider() throws Exception {
+		if (null == platformSensorInfoProvider) {
+			platformSensorInfoProvider = new IbmJava6PlatformSensorInfoProvider();
 		}
 	}
 
@@ -54,8 +91,8 @@ public final class PlatformSensorInfoProviderFactory {
 	 * Creates the {@link SunPlatformSensorInfoProvider}.
 	 */
 	private static synchronized void createSunPlatformSensorInfoProvider() {
-		if (null == sunPlatformSensorInfoProvider) {
-			sunPlatformSensorInfoProvider = new SunPlatformSensorInfoProvider();
+		if (null == platformSensorInfoProvider) {
+			platformSensorInfoProvider = new SunPlatformSensorInfoProvider();
 		}
 	}
 
@@ -63,8 +100,8 @@ public final class PlatformSensorInfoProviderFactory {
 	 * Creates the {@link DefaultPlatformSensorInfoProvider}.
 	 */
 	private static synchronized void createDefaultPlatformSensorInfoProvider() {
-		if (null == defaultPlatformSensorInfoProvider) {
-			defaultPlatformSensorInfoProvider = new DefaultPlatformSensorInfoProvider();
+		if (null == platformSensorInfoProvider) {
+			platformSensorInfoProvider = new DefaultPlatformSensorInfoProvider();
 		}
 	}
 }
--- Agent/Agent/src/info/novatec/inspectit/agent/sensor/platform/provider/sun/SunOperatingSystemInfoProvider.java
+++ Agent/Agent/src/info/novatec/inspectit/agent/sensor/platform/provider/sun/SunOperatingSystemInfoProvider.java
@@ -1,6 +1,7 @@
 package info.novatec.inspectit.agent.sensor.platform.provider.sun;
 
 import info.novatec.inspectit.agent.sensor.platform.provider.OperatingSystemInfoProvider;
+import info.novatec.inspectit.agent.sensor.platform.provider.util.CpuUsageCalculator;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
@@ -113,101 +114,5 @@ public class SunOperatingSystemInfoProvider implements OperatingSystemInfoProvid
 		cpuCalculator.updateCpuUsage();
 
 		return cpuCalculator.getCpuUsage();
-	}
-
-	/**
-	 * This class is used to calculate the cpu usage of the underlying Virtual Machine.
-	 * 
-	 * @author Eduard Tudenhoefner
-	 * 
-	 */
-	private static class CpuUsageCalculator {
-		/**
-		 * The uptime.
-		 */
-		private long uptime = -1L;
-
-		/**
-		 * The process cpu time.
-		 */
-		private long processCpuTime = -1L;
-
-		/**
-		 * The available processors.
-		 */
-		private int availableProcessors = 0;
-
-		/**
-		 * The previous uptime of the Virtual Machine.
-		 */
-		private long prevUptime = 0L;
-
-		/**
-		 * The previous processCpuTime.
-		 */
-		private long prevProcessCpuTime = 0L;
-
-		/**
-		 * The cpu usage.
-		 */
-		private float cpuUsage = 0.0F;
-
-		/**
-		 * Gets {@link #cpuUsage}.
-		 * 
-		 * @return {@link #cpuUsage}
-		 */
-		public float getCpuUsage() {
-			return cpuUsage;
-		}
-
-		/**
-		 * Sets {@link #uptime}.
-		 * 
-		 * @param uptime
-		 *            New value for {@link #uptime}
-		 */
-		public void setUptime(long uptime) {
-			this.uptime = uptime;
-		}
-
-		/**
-		 * Sets {@link #processCpuTime}.
-		 * 
-		 * @param processCpuTime
-		 *            New value for {@link #processCpuTime}
-		 */
-		public void setProcessCpuTime(long processCpuTime) {
-			this.processCpuTime = processCpuTime;
-		}
-
-		/**
-		 * Sets {@link #availableProcessors}.
-		 * 
-		 * @param availableProcessors
-		 *            New value for {@link #availableProcessors}
-		 */
-		public void setAvailableProcessors(int availableProcessors) {
-			this.availableProcessors = availableProcessors;
-		}
-
-		/**
-		 * Calculates the current cpuUsage in percent.
-		 * 
-		 * elapsedCpu is in ns and elapsedTime is in ms. cpuUsage could go higher than 100% because
-		 * elapsedTime and elapsedCpu are not fetched simultaneously. Limit to 99% to avoid showing
-		 * a scale from 0% to 200%.
-		 * 
-		 */
-		public void updateCpuUsage() {
-			if (prevUptime > 0L && this.uptime > prevUptime) {
-				long elapsedCpu = this.processCpuTime - prevProcessCpuTime;
-				long elapsedTime = this.uptime - prevUptime;
-
-				cpuUsage = Math.min(99F, elapsedCpu / (elapsedTime * 10000F * this.availableProcessors));
-			}
-			this.prevUptime = this.uptime;
-			this.prevProcessCpuTime = this.processCpuTime;
-		}
 	}
 }
