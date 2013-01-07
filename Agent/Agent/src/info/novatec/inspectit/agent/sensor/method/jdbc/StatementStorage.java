88e88d652bdaaa467ecdfa11653cd99cfba37bc7
Stefan Siegl <stefan.siegl@novatec-gmbh.de>
2013-01-07 08:54:23 +0100

    INSPECTIT-615: Improve concurrency in statement storage and optimize for "no-bind-value" JDBC configuration.

--- Agent/Agent/src/info/novatec/inspectit/agent/sensor/method/jdbc/StatementStorage.java
+++ Agent/Agent/src/info/novatec/inspectit/agent/sensor/method/jdbc/StatementStorage.java
@@ -5,19 +5,20 @@ import info.novatec.inspectit.util.ThreadLocalStack;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 /**
  * Stores the mapping between statements and objects so that these statements are later accessible.
  * 
  * @author Patrice Bouillet
- * 
+ * @author Stefan Siegl
  */
 public class StatementStorage {
 
@@ -26,21 +27,21 @@ public class StatementStorage {
 	 */
 	private static final Logger LOGGER = Logger.getLogger(StatementStorage.class.getName());
 
-	/**
-	 * The size of the prepared statement map.
-	 */
-	private static final int MAP_SIZE = 50;
+	/** representation of a null value. */
+	private static final String NULL_VALUE = "null";
 
 	/**
-	 * The statement map contains a mapping between the prepared statement objects and their created
-	 * query string with parameter placeholders in it.
+	 * This cache keeps track of the prepared statement objects and associates these with the
+	 * concrete query string and its the bound parameters. Weak keys ensure that elements will be
+	 * cleared regularly. As the key is the PreparedStatement object, as soon as the application
+	 * looses the reference to this object (which is usually very quickly after the invocation), the
+	 * garbage collector can remove this object and thus the entry in the cache. In order to
+	 * safe-guard our cache, elements will also be removed if they are not used for 20 minutes.
+	 * 
+	 * <b> Note that this data structure provides atomic access like a <code>ConcurrentMap</code>.
+	 * </b>.
 	 */
-	private Map<Object, String> preparedStatementMap = new WeakHashMap<Object, String>(MAP_SIZE);
-
-	/**
-	 * Stores the parameters of prepared statements.
-	 */
-	private Map<Object, String[]> parameterMap = new WeakHashMap<Object, String[]>(MAP_SIZE);
+	private Cache<Object, QueryAndParameters> preparedStatements = CacheBuilder.newBuilder().expireAfterAccess(20, TimeUnit.MINUTES).weakKeys().build();
 
 	/**
 	 * Returns the sql thread local stack.
@@ -55,21 +56,7 @@ public class StatementStorage {
 	 */
 	public void addPreparedStatement(Object object) {
 		String sql = sqlThreadLocalStack.getLast();
-
-		synchronized (this) {
-			preparedStatementMap.put(object, sql);
-
-			char[] sqlChars = sql.toCharArray();
-			int count = 0;
-			for (int i = 0; i < sqlChars.length; i++) {
-				char c = sqlChars[i];
-				if ('?' == c) {
-					count = count + 1;
-				}
-			}
-
-			parameterMap.put(object, new String[count]);
-		}
+		preparedStatements.put(object, new QueryAndParameters(sql));
 
 		if (LOGGER.isLoggable(Level.FINER)) {
 			LOGGER.finer("Recorded preparded sql statement: " + sql);
@@ -81,14 +68,22 @@ public class StatementStorage {
 	 * 
 	 * @param object
 	 *            The object which will be used to look up in the map.
-	 * @return The sql string.
+	 * @return The sql string or <code>null</code> if this statement is not available within the
+	 *         storage.
 	 */
 	protected String getPreparedStatement(Object object) {
-		if (LOGGER.isLoggable(Level.FINER)) {
-			LOGGER.finer("Return preparded sql statement: " + preparedStatementMap.get(object));
+		QueryAndParameters queryAndParameters = preparedStatements.getIfPresent(object);
+
+		String query = null;
+		if (null != queryAndParameters) {
+			query = queryAndParameters.query;
 		}
 
-		return preparedStatementMap.get(object);
+		if (LOGGER.isLoggable(Level.FINER)) {
+			LOGGER.finer("Return preparded sql statement: " + query);
+		}
+
+		return query;
 	}
 
 	/**
@@ -96,22 +91,15 @@ public class StatementStorage {
 	 * 
 	 * @param object
 	 *            The object which will be used to look up in the map.
-	 * @return The list of parameters.
+	 * @return The list of parameters or <code> null </code> if there is no container for the given
+	 *         SQL statement or there are no parameters captured within this SQL statement.
 	 */
 	protected List<String> getParameters(Object object) {
-		String[] params = parameterMap.get(object);
-		if (null != params) {
-			if (LOGGER.isLoggable(Level.FINER)) {
-				LOGGER.finer("Return preparded sql statement parameters: " + Arrays.toString(params));
-			}
-
-			List<String> paramList = new ArrayList<String>(params.length);
-			for (String param : params) {
-				paramList.add(param);
-			}
-			return paramList;
-		} else {
+		QueryAndParameters queryAndParameters = preparedStatements.getIfPresent(object);
+		if (null == queryAndParameters) {
 			return null;
+		} else {
+			return queryAndParameters.getParametersAsList();
 		}
 	}
 
@@ -126,34 +114,37 @@ public class StatementStorage {
 	 *            The value to be inserted.
 	 */
 	protected void addParameter(Object preparedStatement, int index, Object value) {
-		String[] parameters = parameterMap.get(preparedStatement);
+		QueryAndParameters queryAndParameters = preparedStatements.getIfPresent(preparedStatement);
 
-		if (null != parameters) {
-			if (parameters.length <= index) {
-				if (LOGGER.isLoggable(Level.WARNING)) {
-					LOGGER.warning("Trying to set the parameter with value " + value + " at index " + index + ", but the prepared statement did not have this parameter.");
-				}
-				return;
-			}
-
-			if (null != value) {
-				if (value instanceof String || value instanceof Date || value instanceof Time || value instanceof Timestamp) {
-					parameters[index] = "'" + value.toString() + "'";
-				} else {
-					parameters[index] = value.toString();
-				}
-			} else {
-				value = "null";
-				parameters[index] = (String) value;
-			}
-
-			if (LOGGER.isLoggable(Level.FINE)) {
-				LOGGER.finer("Prepared Statement :: Added value:" + value.toString() + " with index:" + index + " to prepared statement:" + preparedStatement);
-			}
-		} else {
+		if (null == queryAndParameters) {
 			if (LOGGER.isLoggable(Level.FINE)) {
 				LOGGER.fine("Could not get the prepared statement from the cache to add a parameter! Prepared Statement:" + preparedStatement + " index:" + index + " value:" + value);
 			}
+			return;
+		}
+
+		String[] parameters = queryAndParameters.getParameters();
+
+		if (parameters.length <= index) {
+			if (LOGGER.isLoggable(Level.WARNING)) {
+				LOGGER.warning("Trying to set the parameter with value " + value + " at index " + index + ", but the prepared statement did not have this parameter.");
+			}
+			return;
+		}
+
+		if (null != value) {
+			if (value instanceof String || value instanceof Date || value instanceof Time || value instanceof Timestamp) {
+				parameters[index] = "'" + value.toString() + "'";
+			} else {
+				parameters[index] = value.toString();
+			}
+		} else {
+			value = NULL_VALUE;
+			parameters[index] = (String) value;
+		}
+
+		if (LOGGER.isLoggable(Level.FINE)) {
+			LOGGER.finer("Prepared Statement :: Added value:" + value.toString() + " with index:" + index + " to prepared statement:" + preparedStatement);
 		}
 	}
 
@@ -164,14 +155,16 @@ public class StatementStorage {
 	 *            The prepared statement for which all parameters are going to be cleared.
 	 */
 	protected void clearParameters(Object preparedStatement) {
-		if (parameterMap.containsKey(preparedStatement)) {
-			String[] parameters = parameterMap.get(preparedStatement);
-			parameterMap.put(preparedStatement, new String[parameters.length]);
-		} else {
+		QueryAndParameters queryAndParameters = preparedStatements.getIfPresent(preparedStatement);
+
+		if (null == queryAndParameters) {
 			if (LOGGER.isLoggable(Level.FINE)) {
 				LOGGER.fine("Could not get the prepared statement from the cache to clear the parameters! Prepared Statement:" + preparedStatement);
 			}
+			return;
 		}
+
+		queryAndParameters.clearParameters();
 	}
 
 	/**
@@ -197,4 +190,106 @@ public class StatementStorage {
 		sqlThreadLocalStack.pop();
 	}
 
+	/**
+	 * Value container to store the SQL query and its parameters within the cache of prepared
+	 * statements. The JDBC sensor in inspectIT allows for two modes. The SQL query can be enhanced
+	 * with the values of the bind parameters. This happens if and only if
+	 * "SQL Prepared Statement Parameter Replacement" is set. Thus this container ensures that this
+	 * calculation is only done if this is needed, that is when the parameters are first accessed.
+	 * Thus ensure that you only access the parameters if you really want to fill them!
+	 * 
+	 * <p>
+	 * To access the parameters during the "filling stage", prefer the
+	 * <code> public String[] getParameters() </code> method as this allows to access the internal
+	 * String[].
+	 * 
+	 * @author Stefan Siegl
+	 */
+	private static class QueryAndParameters {
+		/** the SQL query. */
+		private String query;
+
+		/**
+		 * internal container of the SQL bind values. The size of this array defines the number of
+		 * bind values. This field is filled on first access.
+		 */
+		private String[] parameters = null;
+
+		/**
+		 * Creates a new instance of this value container. Please note that creating an instance of
+		 * this class does not calculate the number of available bind values (a.k.a. "parameters").
+		 * They are creating on first use.
+		 * 
+		 * @param query
+		 *            The SQL query.
+		 */
+		public QueryAndParameters(String query) {
+			this.query = query;
+		}
+
+		/**
+		 * Returns the String[] representation of the bind values of this SQL query. This method
+		 * should be used over the <code>List<String> getParametersAsList</code> method to fill the
+		 * parameters as this method provides access to the backing String[] and is thus more
+		 * efficient.
+		 * 
+		 * <b> please note that the calculation of the number of parameters within the SQL query is
+		 * done with the first access to this method. Thus only call this method if you know that
+		 * you do have parameters to set, else there will be unnecessary calculations. </b>
+		 * 
+		 * @return <code>String[]</code> containing the current bind values of this SQL query. The
+		 *         size of the array can be used to deduce the number of available bind parameters
+		 *         based on the SQL query.
+		 */
+		public String[] getParameters() {
+			if (null == parameters) {
+				// Calculate the amount of parameters based on the SQL query. We calculate this
+				// value on first request as this is only needed if we have parameter capturing
+				// active.
+				int count = 0;
+				for (int i = query.length() - 1; i > 0; i--) {
+					if (query.charAt(i) == '?') {
+						count++;
+					}
+				}
+				parameters = new String[count];
+			}
+
+			return parameters; // NOPMD: no copy to improve performance
+		}
+
+		/**
+		 * Resets the bind parameters of this SQL query.
+		 * 
+		 * <b> please note that the calculation of the number of parameters within the SQL query
+		 * will be done as a side-effect of this method (but only if it is not already done before).
+		 * Thus only call this method if you know that you do have parameters to set, else there
+		 * will be unnecessary calculations. </b>
+		 */
+		public void clearParameters() {
+			if (null == parameters) {
+				return;
+			}
+			parameters = new String[parameters.length];
+		}
+
+		/**
+		 * Returns the parameter values as <code>List<String></code>. The list will provide a
+		 * representation of the parameters. Adding to this list will <b> not </b> change the
+		 * parameters. This method is meant to be used from the second after hook to report the
+		 * current parameters.
+		 * <p>
+		 * Please note that to fill the parameters the String[] should be used.
+		 * 
+		 * @return the parameter values as <code>List<String></code> or <code> null </code> if no
+		 *         parameters are captured.
+		 */
+		public List<String> getParametersAsList() {
+			if (null == parameters) {
+				return null;
+			} else {
+				return Arrays.asList(parameters);
+			}
+		}
+	}
 }
