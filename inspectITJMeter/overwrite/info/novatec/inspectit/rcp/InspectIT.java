901ff74cd0f53c135a4759afcf59974f982c3b40
Ivan Senic <ivan.senic@novatec-gmbh.de>
2014-01-27 08:59:10 +0100

    INSPECTIT-1026: Fixed logging for the inspectIT jMeter sampler

--- inspectITJMeter/overwrite/info/novatec/inspectit/rcp/InspectIT.java
+++ inspectITJMeter/overwrite/info/novatec/inspectit/rcp/InspectIT.java
@@ -1,9 +1,15 @@
 package info.novatec.inspectit.rcp;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.access.BeanFactoryLocator;
 import org.springframework.beans.factory.access.BeanFactoryReference;
 import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 
 /**
  * Overwrites the <code>InspectIT</code> class from the inspectIT project. This allows us to re-use
@@ -21,6 +27,8 @@ public class InspectIT {
 	public static BeanFactory beanFactory;
 
 	static {
+		initLogger();
+
 		BeanFactoryLocator beanFactoryLocator = ContextSingletonBeanFactoryLocator.getInstance();
 		BeanFactoryReference beanFactoryReference = beanFactoryLocator.useBeanFactory("ctx");
 		beanFactory = beanFactoryReference.getFactory();
@@ -30,6 +38,22 @@ public class InspectIT {
 	 * The shared instance.
 	 */
 	private static InspectIT plugin = new InspectIT();
+
+	/**
+	 * Initializes the logger.
+	 */
+	private static void initLogger() {
+		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+		try {
+			JoranConfigurator configurator = new JoranConfigurator();
+			configurator.setContext(context);
+			context.reset();
+			configurator.doConfigure(InspectIT.class.getResourceAsStream("/config/logback.xml"));
+		} catch (JoranException je) { // NOPMD NOCHK StatusPrinter will handle this NOCHK
+		}
+		StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+	}
 
 	/**
 	 * Returns a service, if one is registered with the bundle context.
